456fe9f7b94d7b77f05b268c878f3345defac179
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-03-09 23:04:00 +0530

    commti on main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
 import asyncio
+intents = discord.Intents.all()
 TOKEN = "ODE4ODk0MzIwMTY1ODQ3MDQx.YEesxA.pHVyHcEhb600-BVR9omFCHu4sfI"
-client = commands.Bot(command_prefix = ['H!','h!'],intents = discord.Intents.all())
+client = commands.Bot(command_prefix = ['H!','h!'],intents = intents)
 intents.messages = True
 intents.members = True
 @client.event
