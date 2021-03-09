1c2bb2dc566017ee83e1705d8663f557036ec4d5
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-03-09 22:56:03 +0530

    commit on main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+import discord
+from discord.ext import commands
+import asyncio
+TOKEN = "ODE4ODk0MzIwMTY1ODQ3MDQx.YEesxA.pHVyHcEhb600-BVR9omFCHu4sfI"
+client = commands.Bot(command_prefix = ['H!','h!',intents = discord.Intents.all())
+intents.messages = True
+intents.members = True
+@client.event
+async def on_ready:
+    print("Now Online!")
+client.run(TOKEN)
