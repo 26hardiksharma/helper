6622c1220ad25bb92ad6c48a5d29a1fe27cfbcb6
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-03-10 07:52:07 +0530

    commit on main

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ async def on_ready():
     print("Now Online!")
 @client.event
 async def on_member_update(before,after):
-    log = client.get_channel(818899394719252543)
+    logch = client.get_channel(818899394719252543)
     if before.name != after.name:
-        await log.send(f"{before.name}#{before.discriminator} Has Changed Their Name To {after.name}#{after.discriminator}")
-
+        await logch.send(f"{before.name}#{before.discriminator} Has Changed Their Name To {after.name}#{after.discriminator}")
+    elif before.avatar_url != after.avatar_url:
+        embed = discord.Embed(title = f"{after.name}",description = f"{after.mention} Has Updated Their Avatar")
+        embed.add_field(name = "Avatar",value = f"[Current]({after.avatar_url}) 𒌋━━━ [Before]({before.avatar_url})")
+        await logch.send(embed=embed)
 
 client.run(TOKEN)
