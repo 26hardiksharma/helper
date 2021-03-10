43fbb7536ebb925122ba986dc0f9c9e73dbc76b6
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-03-10 14:39:46 +0530

    commit on main

--- main.py
+++ main.py
@@ -14,11 +14,18 @@ async def on_ready():
 async def on_user_update(before,after):
     logch = client.get_channel(818899394719252543)
     if before.name != after.name:
-        await logch.send(f"{before.name}#{before.discriminator} Has Changed Their Name To {after.name}#{after.discriminator}")
+        await logch.send(f"**{before.name}#{before.discriminator}** Has Changed Their Name To **{after.name}#{after.discriminator}**")
     elif before.avatar_url != after.avatar_url:
         embed = discord.Embed(title = f"{after.name}",description = f"{after.mention} Has Updated Their Avatar")
         embed.add_field(name = "Avatar",value = f"[Current]({after.avatar_url}) 𒌋━━━ [Before]({before.avatar_url})")
         embed.set_thumbnail(url = after.avatar_url)
         await logch.send(embed=embed)
+@client.event
+async def on_member_join(member):
+    logch = client.get_channel(818899394719252543)
+    role = discord.utils.get(member.guild.roles, name ="➵MEMBERS")
+    await member.add_roles(role)
+    await logch.send(f"**{member.name}#{member.discriminator}**[ID = {member.id}] Has Joined The Server, {member.guild.member_count}th Member To Join\nTheir Account Was Created At {member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC")}")
+
 
 client.run(TOKEN)
