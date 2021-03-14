0f210eba9c6053edd586e3e4cf3262d12b6164cd
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-03-14 18:27:53 +0530

    commit

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ async def modapply(ctx):
     answers = []
     def check(m):
         return m.author == ctx.author and m.channel == ctx.channel
-    await ctx.send("**Rules To Agree Before You Answer All The Questions:-\n1. All Your Answers Must Be Accurate And Question Related.\n2. You Will Be Given 1 Minute To Answer 1 Question. If You Fail To Answer Within The Time, The Application Will Be Closed By Itself And Your Answers Will Not Be Stored!\n4. You Must Agree To Be Able To Join A VC Before You Are Given The Reputed Supreme Roles.**")
+    await ctx.send("Rules To Agree Before You Answer All The Questions:-\n**1. All Your Answers Must Be Accurate And Question Related.\n2. You Will Be Given 1 Minute To Answer 1 Question. If You Fail To Answer Within The Time, The Application Will Be Closed By Itself And Your Answers Will Not Be Stored!\n3. You Must Agree To Be Able To Join A VC Before You Are Given The Reputed Supreme Roles.**")
     await ctx.send(f"Questions Will Be Sent To You After 20 Seconds..")
     await asyncio.sleep(20)
 
