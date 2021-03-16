eadd47da861f9f243aa07735353ba5978f3b8a8f
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-03-16 21:33:34 +0530

    commit on main

--- main.py
+++ main.py
@@ -310,5 +310,12 @@ async def tag(ctx,*,tag = None):
         await ctx.send("List Of Available Tags :- \n\n**`Furious`**")
     else:
         if tag.lower() == "furious":
-            await ctx.send(f"**___Furious___**\n\n**1). What Is Furious And Whats It's Purpose ?**\n\nFurious Is A Discord Bot Created By {owner.name}#{owner.discriminator} Designed To Moderate And Manage Your Server(s)!\nIt Serves In More Than 130 Servers And Has More Than 30k Users ;)\n\n**2). How Do Add Furious To My Server ?**\n\n To Add Furious To Your Server, Please Follow This Link\n\n**https://discord.com/api/oauth2/authorize?client_id=790478502909837333&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2F4DqmNbUTXa&scope=bot**")
+            await ctx.send(f"**___Furious___**\n\n**1). What Is Furious And Whats It's Purpose ?**\n\nFurious Is A Discord Bot Created By {owner.name}#{owner.discriminator} Designed To Moderate And Manage Your Server(s)!\nIt Serves In More Than 130 Servers And Has More Than 30k Users ;)\n\n**2). How Do Add Furious To My Server ?**\n\nTo Add Furious To Your Server, Please Follow This Link\n\n**https://discord.com/api/oauth2/authorize?client_id=790478502909837333&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2F4DqmNbUTXa&scope=bot**")
+@client.command()
+async def upload_emoji(ctx,name):
+    if ctx.author.guild_permissions.manage_emojis:
+        if message.attachments:
+            image = message.attachments[0]
+            await ctx.guild.create_custom_emoji(name = name,image = image)
+            await ctx.send(f"Success!")
 client.run(TOKEN)
