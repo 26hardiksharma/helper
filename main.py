4ed8163e446406de7916b79f686663a010356d83
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-03-29 16:26:47 +0530

    commit on main

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ async def on_message(message):
                 await message.author.send(f"You have Been Unmuted In {message.guild.name}\nReason :- Mute Duration Expired")
         else:
             if message.channel.id ==826043636063273010:
-                response = rs.get_ai_response(message.content.lower())
+                response = await rs.get_ai_response(message.content.lower())
                 await message.reply(response)
     await client.process_commands(message)
 
