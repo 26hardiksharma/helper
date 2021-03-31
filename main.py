e82557eeb8bdad25e5fae43f613e519602b29a5a
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-03-31 22:50:11 +0530

    commit on main

--- main.py
+++ main.py
@@ -164,6 +164,9 @@ async def on_message(message):
                 await asyncio.sleep(600)
                 await message.author.remove_roles(muted)
                 await message.author.send(f"You have Been Unmuted In {message.guild.name}\nReason :- Mute Duration Expired")
+        elif "<@!757589836441059379>" in message.content.lower():
+            await ctx.send("The Person You Have Mentioned Has Left Discord, Please Try Again Later :)")
+            
         else:
             if message.channel.id ==826043636063273010:
                 if message.is_system():
