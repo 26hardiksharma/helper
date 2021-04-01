9046cb9cf0ee3d653a570a6308d80f49f5d7833c
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-01 09:01:03 +0530

    commit on main

--- main.py
+++ main.py
@@ -385,6 +385,11 @@ async def on_guild_channel_update(before, after):
         embed.add_field(name = "Topic [After]",value = after.topic,inline = False)
         embed.add_field(name = "Responsible User",value = f"{member.name}#{member.discriminator}")
         await logs.send(embed=embed)
+    elif before.type != after.type:
+        embed.add_field(name = "Type[Before]",value = str(before.type).capitalize())
+        embed.add_field(name = "Type[After]",value = str(after.type).capitalize())
+        embed.add_field(name = "Responsible User",value = f"{member.name}#{member.discriminator}")
+        await logs.send(emmbed=embed)
 @client.event
 async def on_member_remove(member):
     logs = client.get_channel(818899394719252543)
