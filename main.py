4ec5f06d889fa691f3924ee8e0df2d66df36160c
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-05 10:24:38 +0530

    commit on main

--- main.py
+++ main.py
@@ -525,8 +525,9 @@ async def toast(ctx,member : discord.Member = None):
     asset = member.avatar_url_as(size = 256)
     data = BytesIO(await asset.read())
     pfp = Image.open(data)
-    pfp = pfp.resize((300,300))
+    pfp = pfp.resize((500,500))
     okay.paste(pfp,(331,388))
     okay.save("hello.png")
     await ctx.send(file = discord.File("hello.png"))
+    #ok
 client.run(TOKEN)
