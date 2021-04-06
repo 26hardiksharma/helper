4e06f5a18be4e725037e2eeb4fb2413c12e26a75
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-06 21:41:09 +0530

    commit on main

--- main.py
+++ main.py
@@ -158,17 +158,17 @@ async def on_message(message):
                 text = '<@&829006631026425896>, Karuta Has Dropped 4 Cards, Quickly Grab Them Before They Expire! Expires In `60` Seconds'
                 msg = await message.channel.send(text)
                 await asyncio.sleep(10)
-                await message.edit('<@&829006631026425896>, Karuta Has Dropped 4 Cards, Quickly Grab Them Before They Expire! Expires In `50 Seconds`')
+                await msg.edit('<@&829006631026425896>, Karuta Has Dropped 4 Cards, Quickly Grab Them Before They Expire! Expires In `50 Seconds`')
                 await asyncio.sleep(10)
-                await message.edit('<@&829006631026425896>, Karuta Has Dropped 4 Cards, Quickly Grab Them Before They Expire! Expires In `40 Seconds`')
+                await msg.edit('<@&829006631026425896>, Karuta Has Dropped 4 Cards, Quickly Grab Them Before They Expire! Expires In `40 Seconds`')
                 await asyncio.sleep(10)
-                await message.edit('<@&829006631026425896>, Karuta Has Dropped 4 Cards, Quickly Grab Them Before They Expire! Expires In `30 Seconds`')
+                await msg.edit('<@&829006631026425896>, Karuta Has Dropped 4 Cards, Quickly Grab Them Before They Expire! Expires In `30 Seconds`')
                 await asyncio.sleep(10)
-                await message.edit('<@&829006631026425896>, Karuta Has Dropped 4 Cards, Quickly Grab Them Before They Expire! Expires In `20 Seconds`')
+                await msg.edit('<@&829006631026425896>, Karuta Has Dropped 4 Cards, Quickly Grab Them Before They Expire! Expires In `20 Seconds`')
                 await asyncio.sleep(10)
-                await message.edit('<@&829006631026425896>, Karuta Has Dropped 4 Cards, Quickly Grab Them Before They Expire! Expires In `10 Seconds`')
+                await msg.edit('<@&829006631026425896>, Karuta Has Dropped 4 Cards, Quickly Grab Them Before They Expire! Expires In `10 Seconds`')
                 await asyncio.sleep(10)
-                await message.edit('The Cards Dropped Have Expired And Can No Longer Be Grabbed')
+                await msg.edit('The Cards Dropped Have Expired And Can No Longer Be Grabbed')
 
     else:
         if "discord.gg/" in message.content.lower():
