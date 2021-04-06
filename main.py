997b5219a3e8349ec91320383b2f8908d5d67189
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-06 21:42:16 +0530

    commit on main

--- main.py
+++ main.py
@@ -169,6 +169,7 @@ async def on_message(message):
                 await msg.edit('<@&829006631026425896>, Karuta Has Dropped 4 Cards, Quickly Grab Them Before They Expire! Expires In `10 Seconds`')
                 await asyncio.sleep(10)
                 await msg.edit('The Cards Dropped Have Expired And Can No Longer Be Grabbed')
+                
 
     else:
         if "discord.gg/" in message.content.lower():
