aba2788b35c3e048c624863bf36e05c70ca54001
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-07 15:46:19 +0530

    commit on main

--- main.py
+++ main.py
@@ -639,7 +639,7 @@ async def on_guild_role_update(before,after):
             embed.add_field(name = "Responsible User",value = user,inline = False)
             await logs.send(embed = embed)
 @client.command()
-async def multikick(ctx,member : discord.Member):
+async def multikick(ctx,*,member : discord.Member):
     if ctx.author.guild_permissions.kick_members:
         haha =""
         okay = ""
