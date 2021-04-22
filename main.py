5611796cfb9ff28f8ce86d3790909b0fab46b6b8
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-22 17:40:04 +0530

    commit on main

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ pkmn = {
 pk = ['moltres','zapdos','mew','mewtwo','raikou','suicune','entei','lugia','ho-oh','regirock','regice','latias','latios','kyogre','groudon','rayquaza','jirachi','uxie','azelf','mesprit','dialga','giratina','heatran','regigigas','creselia','phione','manaphy','darkrai','shaymin','arceus','zekrom','kyurem','lunala']
 @client.event
 async def on_ready():
-    await client.change_presence(activity = discord.Gamer(name = "At Furious's Official Server"))
+    await client.change_presence(activity = discord.Game(name = "At Furious's Official Server"))
     print("Now Online!")
 @client.event
 async def on_user_update(before,after):
