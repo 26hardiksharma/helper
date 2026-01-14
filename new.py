34219af8aa9958e8c2e188a1a5c3a1ea2d294a16
Hardik Sharma <74042237+26hardiksharma@users.noreply.github.com>
2026-01-14 23:28:45 +0530

    Stash new.py
    Comment out the entire content of new.py.

--- new.py
+++ new.py
@@ -1,10 +1,10 @@
-import discord
-from discord.ext import commands
-client = commands.Bot(command_prefix=",",help_command = None)
+# import discord
+# from discord.ext import commands
+# client = commands.Bot(command_prefix=",",help_command = None)
 
-TOKEN = "ODE4ODk0MzIwMTY1ODQ3MDQx.YEesxA.pHVyHcEhb600-BVR9omFCHu4sfI"
+# "
 
-@client.command()
-async def new(ctx):
-    await ctx.send("File new.py Is Working Along With File main.py")
-client.run(TOKEN)
+# @client.command()
+# async def new(ctx):
+#     await ctx.send("File new.py Is Working Along With File main.py")
+# client.run(TOKEN)
